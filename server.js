190c71f741aa24877d25184e4c73cbd9878c57aa
tsah012 <tsah012@gmail.com>
2021-06-20 19:50:18 +0300

    Add config file
    Signed-off-by: tsah012 <tsah012@gmail.com>

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const path = require('path');
+const config = require("./configuration_settings")
 const bodyParser = require('body-parser');
 const express = require('express');
-const port = 3000;
 const server = express();
+
+
 server.use(express.static(path.join(__dirname, 'public')));
 
 
-server.listen(port, function (){
-    console.log("listening on port", port)
+server.listen(config.port, function (){
+    console.log("listening on port", config.port)
 });

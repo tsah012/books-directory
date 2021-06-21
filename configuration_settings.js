14328acbf8da44475bad74ace32c4b3647922769
tsah012 <tsah012@gmail.com>
2021-06-21 21:19:41 +0300

    open connection to db when starting server
    Signed-off-by: tsah012 <tsah012@gmail.com>

--- configuration_settings.js
+++ configuration_settings.js
@@ -1,3 +1,5 @@
 module.exports = {
-    port:3000
+    port:3000,
+    dbConnectionString:"mongodb://localhost:27017/",
+    dbName:"booksdir"
 }
--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-const path = require('path');
 const config = require("./configuration_settings")
+const path = require('path');
+const mongodb = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const express = require('express');
+
+
 const server = express();
-
-
 server.use(express.static(path.join(__dirname, 'public')));
 
+// Initialize connection with db and keep it opened
+mongodb.connect(config.dbConnectionString + config.dbName, function (err, database) {
+    if (err) throw err;
 
-server.listen(config.port, function (){
-    console.log("listening on port", config.port)
+    // Save reference to db connection for DAL use
+    server.locals.db = database;
+    server.listen(config.port, function () {
+        console.log("listening on port", config.port)
+    });
 });
+
+
+

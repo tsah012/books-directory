a1604080cbbf4f41fc1eb3bbb8560b0d0f5ae3b6
tsah012 <tsah012@gmail.com>
2021-07-04 19:19:25 +0300

    change http status to 201 when registration ended successfully
    Signed-off-by: tsah012 <tsah012@gmail.com>

--- custom_modules/routers/users.js
+++ custom_modules/routers/users.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post('/user/add', async function (req, res, next) {
     try {
         let user = await usersDAL.addUser(req.body.name, req.body.mail, req.body.password);
-        res.send({status: true, message: 'Request ended successfully'});
+        res.status(httpStatusCodes.CREATED).send({status: true, message: 'Request ended successfully'});
     }
     catch (err) {
         next(err);
